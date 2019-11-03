e0848128726b84bad6b39030b679d696002c8172
Weidong Zhang <william_zwd@outlook.com>
2019-11-03 16:31:16 -0500

    ISBN and test cases

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,87 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { ReactiveFormsModule, FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
+import { ApplicationModule } from '@angular/core';
+
+let fixture: ComponentFixture<AppComponent>;
+let component: AppComponent;
 
 describe('AppComponent', () => {
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        ReactiveFormsModule, 
+        FormsModule
       ],
       declarations: [
         AppComponent
       ],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
   }));
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'ISBN'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('ISBN');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ISBN app is running!');
+  it('ISBN "780470059029" should return true', () =>
+  {
+    expect(component.checkISBN("9780470059029")).toEqual(true);
+  });
+
+  it ('ISBN "978 0 471 48648 0" should return true', () =>
+  {
+    expect(component.checkISBN("978 0 471 48648 0")).toEqual(true);
+  });
+
+  it('ISBN "978-0596809485" should return true', () =>
+  {
+    expect(component.checkISBN("978-0596809485")).toEqual(true);
+  });
+
+  it('ISBN "978-0-13-149505-0" should return true', () =>
+  {
+    expect(component.checkISBN("978-0-13-149505-0")).toEqual(true);
+  });
+
+  it('ISBN "978-0-262-13472-9" should return true', () =>
+  {
+    expect(component.checkISBN("978-0-262-13472-9")).toEqual(true);
+  });
+
+  it('the length of ISBN "" is 0 should return false', () =>
+  {
+    expect(component.checkISBN("")).toEqual(false);
+  });
+
+  it('the length of ISBN "1234567890" is < 13 should return false', () =>
+  {
+    expect(component.checkISBN("1234567890")).toEqual(false);
+  });
+
+  it('the length of ISBN "12345678901234" > 13 should return false', () =>
+  {
+    expect(component.checkISBN("12345678901234")).toEqual(false);
+  });
+ 
+  it('ISBN "111 111 111 111 6" should return true', () =>
+  {
+    expect(component.checkISBN("111 111 111 111 6")).toEqual(true);
+  });
+
+  it('ISBN "111a111 111 111 6" contains character other than number, " ", or "-" should return false', () =>
+  {
+    expect(component.checkISBN("111a111 111 111 6")).toEqual(false);
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,99 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'ISBN';
+  isbnForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder)
+  {
+    this.isbnForm = this.formBuilder.group({
+      isbn: ['']
+    });
+  }
+
+  checkISBN(isbn)
+  {
+      // index of ISBN number
+      var index = 0;
+      // sum up ISBN number
+      var sum = 0;
+      // check sum of ISBN
+      var checksum = 0;
+  
+      // for each character of ISBN
+      for (var i = 0; i < isbn.length; i++)
+      {
+          // if the character is a number
+          if (isbn.charAt(i) >= '0' && isbn.charAt(i) <= '9')
+          {
+              // 1 - 12 of ISBN
+              if (index < 12)
+              {
+                  // alternative time 1 and 3
+                  if (index % 2 == 0)
+                  {
+                      sum += parseInt(isbn.charAt(i));
+                  }
+                  else
+                  {
+                      sum += parseInt(isbn.charAt(i)) * 3;
+                  }
+              }
+              // checksum of ISBN
+              else
+              {
+                  checksum = parseInt(isbn.charAt(i));
+              }
+              index++;
+          }
+          // if the character is ' ' or '-'
+          else if (isbn.charAt(i) == ' ' || isbn.charAt(i) == '-')
+          {
+          }
+          // the invalidate character
+          else
+          {
+              return false;
+          }
+      }
+  
+      // ISBN 13
+      if (index == 13)
+      {
+          // checksum
+          if (checksum == ((10 - (sum % 10)) % 10))
+          {
+              return true;
+          }
+      }
+  
+      return false;
+  }
+  
+
+  onSubmit(isbn)
+  {
+    if (isbn.trim() == '')
+    {
+        alert("No ISBN input");
+    }
+    else
+    {
+        if (this.checkISBN(isbn))
+        {
+            alert("It is a valid ISBN");
+        }
+        else
+        {
+            alert("It is not a valid ISBN");
+        }
+    }
+  }
 }
